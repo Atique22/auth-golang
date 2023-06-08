97ad5c79a0f26c335c6dc2c60a342806b3fa389b
atique.ahmad@formsoft-inc.com <65412984+Atique22@users.noreply.github.com>
2023-06-08 13:44:16 +0500

    user authentification

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,9 +1,16 @@
 package controllers
 
 import (
+	"context"
+	"net/http"
+	"time"
+
 	"github.com/atique/auth-golang/database"
+	"github.com/atique/auth-golang/helpers"
+	"github.com/atique/auth-golang/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -28,7 +35,21 @@ func GetUsers() {
 }
 
 func GetUser() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
+	return func(c *gin.Context) {
 		userId := c.Param("user_id")
+		if err := helpers.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
 	}
 }

3327633e4809b6b4285a9818321e22d8601046c9
atique.ahmad@formsoft-inc.com <65412984+Atique22@users.noreply.github.com>
2023-06-08 16:39:35 +0500

    user authentification

--- routes/userRouter.go
+++ routes/userRouter.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/atique/auth-golang/controllers"
+	"github.com/atique/auth-golang/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(incomingRoutes *gin.Engine) {
-	// incomingRoutes.Use(middleware.Authenticate())
+	incomingRoutes.Use(middleware.Authenticate())
 	incomingRoutes.GET("/users", controllers.GetUsers())
 	incomingRoutes.GET("/users/:user_id", controllers.GetUser())
 }
